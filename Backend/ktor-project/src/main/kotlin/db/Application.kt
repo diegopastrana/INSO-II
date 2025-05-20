af7989884c06f2a6bbca95362999688cd272a77a
VictorMartinezVega <vmartv01@estudiantes.unileon.es>
2025-05-20 12:02:42 +0200

    Aplicacion creada

---
+++ Backend/ktor-project/src/main/kotlin/db/Application.kt
@@ -0,0 +1,57 @@
+package com.example
+
+import com.example.config.configureSecurity
+import com.example.config.configureSerialization
+import com.example.config.initDatabase
+import com.example.db.seed.insertInitialGames
+import com.example.db.tables.Carrito
+import com.example.db.tables.PedidoItems
+import com.example.db.tables.Pedidos
+import com.example.db.tables.Usuarios
+import com.example.db.tables.Videojuegos
+import com.example.routes.configureAuthRoutes
+import com.example.routes.configureCartRoutes
+import com.example.routes.configureGameRoutes
+import com.example.routes.configureOrderRoutes
+import com.example.routes.configureTestRoutes
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun main(args: Array<String>) {
+  io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+  configureSerialization()
+  initDatabase()
+
+  transaction {
+    SchemaUtils.create(
+      Videojuegos,
+      Usuarios,
+      Pedidos,
+      PedidoItems,
+      Carrito
+    )
+  }
+  install(CORS) {
+    anyHost()
+    allowCredentials = true
+    allowHeader(HttpHeaders.ContentType)
+    allowMethod(HttpMethod.Get)
+    allowMethod(HttpMethod.Post)
+    allowMethod(HttpMethod.Put)
+    allowMethod(HttpMethod.Delete)
+  }
+
+  insertInitialGames()
+  configureSecurity()
+  configureTestRoutes()
+  configureAuthRoutes()
+  configureGameRoutes()
+  configureCartRoutes()
+  configureOrderRoutes()
+}
