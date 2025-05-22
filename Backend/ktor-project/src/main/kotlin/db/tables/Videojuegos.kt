593e170efa25e28402e630303c48b638cf9a7721
rvillf03 <rvillf03@estudiantes.unileon.es>
2025-05-22 18:38:31 +0200

    Anyadida descripcion a bbdd

--- Backend/ktor-project/src/main/kotlin/db/tables/Videojuegos.kt
+++ Backend/ktor-project/src/main/kotlin/db/tables/Videojuegos.kt
@@ -5,4 +5,5 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 object Videojuegos : IntIdTable("videojuegos") {
     val nombre = varchar("nombre", 255)
     val precio = decimal("precio", 10,2)
+    val description = text("description")
 }
