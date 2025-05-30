70d80e16d7f1083d3d25596d7dfb9e381cce46de
serrgi0 <sgarcy01@estudiantes.unileon.es>
2025-05-30 14:39:17 +0200

    Update DatabaseConfig.kt

--- Backend/ktor-project/src/main/kotlin/config/DatabaseConfig.kt
+++ Backend/ktor-project/src/main/kotlin/config/DatabaseConfig.kt
@@ -4,12 +4,19 @@ import io.ktor.server.util.url
 import org.jetbrains.exposed.sql.Database
 
 fun initDatabase() {
-    val raw = System.getenv("DATA_BASE_URL")!!
-    val jdbcUrl = if (raw.startsWith("jdbc:")) raw else "jdbc:$raw"
+    val host     = System.getenv("DB_HOST")!!
+    val port     = System.getenv("DB_PORT")!!
+    val name     = System.getenv("DB_NAME")!!
+    val user     = System.getenv("DB_USER")!!
+    val password = System.getenv("DB_PASSWORD")!!
+
+    val jdbcUrl = "jdbc:postgresql://$host:$port/$name?sslmode=require"
+
     Database.connect(
         url      = jdbcUrl,
         driver   = "org.postgresql.Driver",
-        user     = System.getenv("DB_USER")!!,
-        password = System.getenv("DB_PASSWORD")!!
+        user     = user,
+        password = password
     )
 }
+
