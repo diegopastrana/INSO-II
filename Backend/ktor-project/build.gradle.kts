83277deb7e876c509c43280b5d30d1ab3265504d
serrgi0 <sgarcy01@estudiantes.unileon.es>
2025-05-31 18:39:30 +0200

    Intento de arreglo despliegue

--- Backend/ktor-project/build.gradle.kts
+++ Backend/ktor-project/build.gradle.kts
@@ -1,9 +1,11 @@
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     kotlin("plugin.serialization") version "1.8.10"
     application
+
+    // =====> Agregamos ShadowJar
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.example"
@@ -27,8 +29,8 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:0.61.0")
     implementation("org.jetbrains.exposed:exposed-dao:0.61.0")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.61.0")
-
     implementation("org.jetbrains.exposed:exposed-java-time:0.61.0")
+
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
 
     implementation("io.ktor:ktor-client-cio:3.1.2")
@@ -42,13 +44,18 @@ dependencies {
     implementation("io.ktor:ktor-server-cors:3.1.2")
 
     implementation("org.postgresql:postgresql:42.7.2")
-
     implementation("com.stripe:stripe-java:29.1.0")
-
     implementation("com.auth0:java-jwt:4.5.0")
-
     implementation("commons-codec:commons-codec:1.18.0")
 
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+}
+
+// Configuración de ShadowJar para producir un JAR “fat” llamado app.jar
+tasks {
+    shadowJar {
+        archiveFileName.set("app.jar")
+        mergeServiceFiles()
+    }
 }
