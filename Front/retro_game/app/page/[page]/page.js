be2bbce2ad09e85005615d807d3660fa675b9fcb
serrgi0 <sgarcy01@estudiantes.unileon.es>
2025-05-31 17:36:04 +0200

    push para control de error

--- Front/retro_game/app/page/[page]/page.js
+++ Front/retro_game/app/page/[page]/page.js
@@ -3,6 +3,7 @@ import ClientHome from "../../clientHome";
 export default async function Page({ params }) {
   const { page } = await params 
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/games?page=${page}&size=12`);
+  console.log(res);
   const data = await res.json();
 
   return <ClientHome initialGames={data} initialPage={page} />;
